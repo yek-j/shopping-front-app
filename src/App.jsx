2133a67feb4a3e5c84fe44aea7d0d64f0c9d45d5
jyk <jeonyk219@gmail.com>
2024-05-08 14:20:49 +0900

    html, css 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from '../component/common/header'
 import './App.css'
 
 function App() {
   return (
     <>
+      <Header/>
       <div>
         <h1>Shopping Mall</h1>
       </div>
