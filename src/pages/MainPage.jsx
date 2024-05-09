cede9df8df591c0265223b957b313e313e402df1
jyk <jeonyk219@gmail.com>
2024-05-09 14:42:17 +0900

    MainPage 추가

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/common/Header'
+import Carousel from '../components/content/Carousel';
+
+function Main() {
+  return (
+    <>
+      <Header/>
+      <Carousel/>
+      <div>
+        <h1>Shopping Mall</h1>
+      </div>
+    </>
+  )
+}
+
+export default Main;
