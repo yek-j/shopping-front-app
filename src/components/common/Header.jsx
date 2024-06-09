5324656809a17a9c832bcfecfc6bc6d8f99bb3f8
JeonYekyeong <jeonyk219@gmail.com>
2024-06-09 21:51:38 +0900

    Header href 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Box, AppBar, Toolbar, Typography, CssBaseline, Button, IconButton, Badge}  from '@mui/material'
+import {Box, Tooltip, AppBar, Toolbar, CssBaseline, Button, IconButton, Badge}  from '@mui/material'
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import PersonIcon from '@mui/icons-material/Person';
 
 const navItems = ["item1", "item2", "item3"];
 
@@ -10,11 +11,9 @@ function Header() {
             <CssBaseline/>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" color="inherit" component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }} >
-                    DevBabys Shop 
-                    </Typography>
-                    
+                    <Button href="/" size="large" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, color: '#fff' }}>
+                          DevBabys Shop 
+                        </Button>
                     <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                         {navItems.map((item) => (
                         <Button key={item} size="large" sx={{ color: '#fff' }}>
@@ -24,11 +23,18 @@ function Header() {
                         <Button href="/login" size="small" color="inherit" sx={{ marginLeft: 10 }}>
                             LOGIN
                         </Button>
-                        <IconButton size="large" aria-label="shopping basket" color="inherit">
-                            <Badge badgeContent={1} color="error">
-                                <ShoppingBasketIcon/>
-                            </Badge>
-                        </IconButton>
+                        <Tooltip title="마이페이지">
+                            <IconButton too href="/mypage" size="large" aria-label="person" color="inherit">
+                                <PersonIcon/>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="장바구니">
+                            <IconButton href="/cart" size="large" aria-label="shopping basket" color="inherit">
+                                <Badge badgeContent={1} color="error">
+                                    <ShoppingBasketIcon/>
+                                </Badge>
+                            </IconButton>
+                        </Tooltip>
                     </Box>
                 </Toolbar>
             </AppBar>
