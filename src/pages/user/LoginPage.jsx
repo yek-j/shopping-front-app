1958ef3c9fefa393c36d9f78bf4a999fb99b5648
JeonYekyeong <jeonyk219@gmail.com>
2024-06-11 21:54:32 +0900

    async로 변경

--- src/pages/user/LoginPage.jsx
+++ src/pages/user/LoginPage.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/common/Header";
 import UserField from "../../components/common/UserField";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import TextDialog from "../../components/common/TextDialog";
 
 
 function LoginPage() {
@@ -10,12 +11,15 @@ function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
+
     const handleChange = (data) => {
         if(data.email) setEmail(data.email);
         if(data.password) setPassword(data.password);
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
         const data = {
@@ -25,23 +29,24 @@ function LoginPage() {
 
         const url = import.meta.env.VITE_API_URL + "/user/login";
 
-        axios.post(url, data)
-            .then(response => {
-                if(response.data.result == "success") {
-                    localStorage.setItem("token", data.value);
-                    navigate('/');
-                } else {
-                    alert("이메일이나 패스워드가 틀렸습니다.");
-                }
-            })
-            .catch(error => {
-                console.error('요청 실패', error);
-            })
+        try {
+            const res = await axios.post(url, data);
+            if(res.data.result == "success") {
+                localStorage.setItem("token", res.data.value);
+                navigate('/');
+            } else {
+                setMessage("이메일이나 패스워드가 틀렸습니다.");
+                setOpen(true);
+            }
+        } catch(e) {
+            console.error('요청 실패', error);
+        }
     }    
 
     return (
         <div>
             <Header/>
+            <TextDialog open={open} message={message} close={() => setOpen(false)} /> 
             <UserField type="login" change={handleChange} submit={handleSubmit} />
         </div>
     )
--- src/pages/user/RegisterPage.jsx
+++ src/pages/user/RegisterPage.jsx
@@ -3,12 +3,16 @@ import axios from "axios";
 import Header from "../../components/common/Header";
 import UserField from "../../components/common/UserField";
 import { useNavigate } from 'react-router-dom';
+import TextDialog from "../../components/common/TextDialog";
 
 function RegisterPage() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
     
     const handleChange = (data) => {
         if(data.email) setEmail(data.email);
@@ -16,7 +20,7 @@ function RegisterPage() {
         if(data.password) setPassword(data.password);
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         
         const data = {
@@ -27,23 +31,24 @@ function RegisterPage() {
     
         const url = import.meta.env.VITE_API_URL + '/user/register';
         
-        axios.post(url, data) 
-            .then(response => {
-                if(response.data.result == "success") {
-                    alert("회원가입 성공");
-                    navigate('/login');
-                } else {
-                    alert(response.data.value);
-                }
-            })
-            .catch(error => {
-                console.error('요청 실패', error);
-            }); 
+        try {
+            const res = await axios.post(url, data);
+            if(res.data.result == "success") {
+                alert("회원가입 성공");
+                navigate('/login');
+            } else {
+                setMessage(res.data.value);
+                setOpen(true);
+            }
+        } catch(e) {
+            console.error('요청 실패', error);
+        }
     }
 
     return(
         <div>
             <Header/>
+            <TextDialog open={open} message={message} close={() => setOpen(false)} /> 
             <UserField type="register" change={handleChange} submit={handleSubmit}/>
         </div>
     );
