1ee786ef9186b18ed69b9f03eea6da42d5ab0a96
jyk <jeonyk219@gmail.com>
2024-08-23 14:00:41 +0900

    updateUser js

--- src/js/user/user.js
+++ src/js/user/user.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getHeader } from "../admin/admin";
 
 export const getUser = async () => {
     const token = JSON.parse(localStorage.getItem('token')).value;
@@ -14,6 +15,23 @@ export const getUser = async () => {
             return res.data.value;
         } else {
             alert("회원 정보를 불러오지 못했습니다.");
+        }
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+export const updateUser = async (data) => {
+    const headers = getHeader('application/json');
+    const url = import.meta.env.VITE_API_URL + '/user/update';
+
+    try {
+        const res = await axios.put(url, data, { headers });
+
+        if(res.data.result == "success") {
+            alert("회원 정보 수정 완료");
+        } else {
+            alert(res.data.value);
         }
     } catch(e) {
         console.error(e);
