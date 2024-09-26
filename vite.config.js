04d23b1b6d8ab434230c9ad89585017cc32ae63e
jyk <jeonyk219@gmail.com>
2024-09-27 08:34:33 +0900

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    __VITE_API_URL__: JSON.stringify(process.env.VITE_API_URL),
+  },
+});
