f57d23c94a9ec3b2a9429d9765320f8befb27d3e
jyk <jeonyk219@gmail.com>
2024-09-27 08:44:56 +0900

    설정 수정

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,11 @@ COPY package*.json ./
 RUN npm install
 
 COPY . .
+
+# 빌드 시 환경 변수 사용
+ARG VITE_API_URL
+ENV VITE_API_URL=${VITE_API_URL}
+
 RUN npm run build
 
 # nginx
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __VITE_API_URL__: JSON.stringify(process.env.VITE_API_URL),
-  },
-});
+})
